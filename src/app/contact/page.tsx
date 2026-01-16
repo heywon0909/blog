a8be05b6f9052ef7f97684f931d84a82907fd06b
heywon0909 <hw731911@gmail.com>
2026-01-16 16:43:02 +0900

    feat: contact route 마크업 작업

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { OpinionForm } from '@/components/OpinionForm';
+
+export default function Page() {
+  return (
+    <div className="w-full md:w-1/3">
+      <h2 className="text-xl font-semibold">Contact me</h2>
+      <OpinionForm />
+    </div>
+  );
+}
