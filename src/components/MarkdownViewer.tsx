a7b5af9b8b7342e68a995cdbe5f6252e5e3f83d9
heywon0909 <hw731911@gmail.com>
2026-01-27 17:37:51 +0900

    refactor: src type string 으로 고정

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -25,7 +25,7 @@ export default function MarkdownViewer({ content }: { content: string }) {
           img: image => (
             <Image
               className="w-full max-h-60 object-cover"
-              src={image.src || ''}
+              src={String(image.src || '')}
               alt={image.alt || ''}
               width={500}
               height={350}
